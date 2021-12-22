0b00f15d2a22bfaf61af0e3243d9e2c58d150b2f
Jamison Hyman <jamison.hyman@instacart.com>
2021-12-21 21:44:12 -0500

    Add projects

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -54,24 +54,54 @@ export let meta: MetaFunction = () => {
   };
 };
 
+const projects = [
+  {
+    img: '/projects/mio.png',
+    title: "My Inspirational Oval (MIO) | A playful way to engage in wellness",
+  },
+  {
+    img: '/projects/bridgecare.png',
+    title: 'BridgeCare | Fostering trust between families and childcare providers',
+  },
+  {
+    img: '/projects/chicagoNews.png',
+    title: 'ABC7 Chicago News | How news can inspire and empower',
+  },
+]
+
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
   let data = useLoaderData<IndexData>();
 
   return (
-    <div className="remix__page">
-      <main>
-        <h2>People, puzzles, and problem solving excite me. I’m delighted to have found my place in UX. </h2>
-        <h4>Hi there, I’m Visnu!</h4>
-        <p>I’m an experience designer with a background in data analysis and business strategy. My passion is solving complex problems and helping people accomplish what is important to them.</p>
-        <p>I love UX because it encompasses many of the things that I’m passionate about and experienced with such as product strategy, data analysis, user research, psychology, and storytelling. I know how to leverage data to create simple and effective designs. I keep business needs in mind while being an advocate for the humans that I’m designing for. Accessibility and inclusion are always top of mind in my design approach.</p>
-        <p>Outside of UX, I am obsessed with dogs, plants, and travel. On a rainy day, you will find me crafting or learning to DJ.</p>
-      </main>
-      <img
-        alt="Visnu Ghosh standing in front of Bay Bridge with dog, Bryn"
-        className="profilePicture"
-        src="/me_profile.png"
-      />
+    <div className="remix__page" style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className="row" style={{ display: 'flex' }}>
+        <div>
+          <h2>People, puzzles, and problem solving excite me. I’m delighted to have found my place in UX. </h2>
+          <h4>Hi there, I’m Visnu!</h4>
+          <p>I’m an experience designer with a background in data analysis and business strategy. My passion is solving complex problems and helping people accomplish what is important to them.</p>
+          <p>I love UX because it encompasses many of the things that I’m passionate about and experienced with such as product strategy, data analysis, user research, psychology, and storytelling. I know how to leverage data to create simple and effective designs. I keep business needs in mind while being an advocate for the humans that I’m designing for. Accessibility and inclusion are always top of mind in my design approach.</p>
+          <p>Outside of UX, I am obsessed with dogs, plants, and travel. On a rainy day, you will find me crafting or learning to DJ.</p>
+        </div>
+        <img
+          alt="Visnu Ghosh standing in front of Bay Bridge with dog, Bryn"
+          className="profilePicture"
+          src="/me_profile.png"
+        />
+      </div>
+      <div className="projects" style={{ display: 'flex', justifyContent: "space-around" }}>
+        {projects.map(project => (
+          <div className="project" key={project.title} style={{ display: 'flex', flexDirection: 'column', border: '1px solid black', alignItems: 'center', marginRight: 20, width: 376, padding: 15 }}>
+            <img
+              alt={project.title}
+              className="projectPreviewImage"
+              src={project.img}
+              style={{ width: '22rem' }}
+            />
+            <span>{project.title}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
